dd95c352637283acb9fd96da087a2f0c4cca9af4
Seamor Estrabon <seamorestrabon@gmail.com>
2024-01-04 05:52:18 +0800

    completed the the fundamentals part 1 assignment

--- javascript-exercises/javascript-fundamentals-1/javascript.js
+++ javascript-exercises/javascript-fundamentals-1/javascript.js
@@ -4,13 +4,11 @@ let admin, name;
 let planetName = "Earth", currentUser = "James";
 
 const BIRTHDAY = '11.12.1996';
-// const age = someCode(BIRTHDAY); //someCode is not defined and will be discuss in the next lesson
 
 name = "John";
 admin = name;
 
 alert(admin);
-
 alert(currentUser + " from " + planetName);
+alert("check console log");
 
-// alert("Age is " + age + " and Birthday is on " + BIRTHDAY);
